117ec717ff5225a07b44c2fbd01793c55872ee59
“Yasith <48174322+yasithKumara@users.noreply.github.com>
2021-06-11 11:57:41 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+/// <reference types="google.maps" />
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  @ViewChild('map',{static: true}) mapElement: ElementRef;
+  map: any;
 
   constructor() {}
 
+  ngOnInit(){
+   this.initMap();
+  }
+
+  initMap() {
+   let latLng =  new google.maps.LatLng(7.453048,79.951090);
+
+   let mapOptions ={
+     center : latLng,
+     zoom: 15,
+     mapTypeId : google.maps.MapTypeId.ROADMAP
+   };
+
+   this.map = new google.maps.Map(this.mapElement.nativeElement ,mapOptions);
+  }
 }
